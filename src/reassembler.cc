0b271b95b167b2d3b142d83dfca6240f13471953
Chris <chris289742@gmail.com>
2026-06-12 01:01:20 +0000

    lab2: reassembler initial implementation

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -1,16 +1,138 @@
 #include "reassembler.hh"
-
+#include <map>
+#include <string>
 using namespace std;
 
 void Reassembler::insert(uint64_t first_index, string data,
-                         bool is_last_substring) {
-  // Your code here.
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
+                         bool is_last_substring)
+{
+  // Process the situations where overlap, close happens.
+  // byte stream: ouput_
+  //
+  const uint64_t first_unassembled_index{output_.writer().bytes_pushed()};
+  const uint64_t first_unacceptable_index{first_unassembled_index + output_.writer().available_capacity()};
+
+  if (is_last_substring)
+  { // to record pos of eof
+    eof_idx_ = first_index + data.size();
+  }
+
+  if (eof_idx_.has_value() && output_.writer().bytes_pushed() >= eof_idx_.value())
+  {
+    output_.writer().close();
+    return;
+  }
+
+  // trim right
+  if (first_index + data.size() > first_unacceptable_index)
+  {
+    if (first_index >= first_unacceptable_index)
+      return;
+    uint64_t keep_len = first_unacceptable_index - first_index; // A simple way to calculate how much size from first index byte to last acceptable byte
+    data = data.substr(0, keep_len);
+  }
+  // trim left
+  if (first_index < first_unassembled_index)
+  {
+    if (first_index + data.size() <= first_unassembled_index)
+    {
+      return;
+    }
+    uint64_t overlap = first_unassembled_index - first_index;
+    data = data.substr(overlap);
+    first_index = first_unassembled_index;
+  }
+
+  // check empty string
+  if (data.empty())
+  {
+    return;
+  }
+
+  // at this point, first_unassembled <= the index of the incoming substring < first_unacceptable
+  // if overlap with substring in buffer_, then ...
+  // overlapping happens at left edge
+  if (first_index > first_unassembled_index)
+  {
+    auto it = buffer_.lower_bound(first_index);
+
+    // if (it != buffer_.end() && it->first == first_index) {
+    //   if (it->second.size() >= data.size()) {
+    //     return; // new data fully redundant; don't overwrite existing
+    //   }
+    //   buffer_.erase(it);              // new data is longer; replace existing
+    //   it = buffer_.lower_bound(first_index);
+    // }
+
+    // trim left
+    if (it != buffer_.begin())
+    {
+      auto prev_it = std::prev(it);
+      uint64_t prev_end = prev_it->first + prev_it->second.size();
+
+      if (prev_end > first_index)
+      {
+        if (prev_end >= first_index + data.size())
+        {
+          return;
+        }
+        uint64_t overlap_len = prev_end - first_index;
+        data = data.substr(overlap_len);
+        first_index = prev_end;
+      }
+    }
+    it = buffer_.lower_bound(first_index); // first index may be modified from left trimming
+    while (it != buffer_.end() && it->first < first_index + data.size())
+    {
+      uint64_t next_end = it->first + it->second.size();
+      if (next_end <= first_index + data.size())
+      {
+        it = buffer_.erase(it);
+      }
+      else
+      {
+        uint64_t new_len = it->first - first_index;
+        data = data.substr(0, new_len);
+        break;
+      }
+    }
+    buffer_[first_index] = data;
+  }
+  else if (first_index == first_unassembled_index)
+  {
+    output_.writer().push(data);
+    while (!buffer_.empty())
+    {
+      auto it = buffer_.begin();
+      uint64_t current_unassembled = output_.writer().bytes_pushed();
+      if (it->first <= current_unassembled)
+      {
+        uint64_t upper_bound = it->first + it->second.size();
+        if (upper_bound > current_unassembled)
+        {
+          uint64_t offset = current_unassembled - it->first;
+          output_.writer().push(it->second.substr(offset));
+        }
+        buffer_.erase(it);
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
+  if (eof_idx_.has_value() && output_.writer().bytes_pushed() >= eof_idx_.value())
+  {
+    output_.writer().close();
+  }
 }
 
-uint64_t Reassembler::bytes_pending() const {
-  // Your code here.
-  return {};
+uint64_t Reassembler::bytes_pending() const
+{
+  uint64_t cnt{0};
+  for (const auto &it : buffer_)
+  {
+    cnt += it.second.size();
+  }
+  return cnt;
 }
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "byte_stream.hh"
+#include <map>
+#include <optional>
+
 
 class Reassembler {
 public:
   // Construct Reassembler to write into given ByteStream.
-  explicit Reassembler(ByteStream &&output) : output_(std::move(output)) {}
-
+  explicit Reassembler(ByteStream &&output) : output_(std::move(output)), buffer_(), eof_idx_() {}
+  
   /*
    * Insert a new substring to be reassembled into a ByteStream.
    *   `first_index`: the index of the first byte of the substring
@@ -41,6 +44,9 @@ public:
   // Access output stream writer, but const-only (can't write from outside)
   const Writer &writer() const { return output_.writer(); }
 
+
 private:
   ByteStream output_; // the Reassembler writes to this ByteStream
+  std::map<uint64_t, std::string> buffer_;
+  std::optional<uint64_t> eof_idx_;
 };
